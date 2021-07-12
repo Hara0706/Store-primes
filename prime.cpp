2c255e9f6c50fa78b2967c1fedc15f599156072a
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-12 17:38:25 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+
+  int a[20];
+
+  int i;
+
+  int prime[20] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71};
+
+  for(i=0; i<20; i++)
+
+  {
+
+      a[i] = prime[i];
+
+  }
+
+
+
+
+   return 0;
+
+}
